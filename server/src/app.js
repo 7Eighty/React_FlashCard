68be4ebc5872ead535134d87dda7046ce3218eb8
7Eighty <igrovoi.ac@yandex.ru>
2025-06-05 16:12:56 +0300

    init

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const morgan = require('morgan');
+const express = require('express');
+const serverConfig = require('./config/serverConfig');
+
+const app = express();
+
+serverConfig(app);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev')); // * подробное логирование запросов между клиентом и сервером в терминале
+app.use(express.urlencoded({ extended: true })); // * читать данные из тела запросов. ! ИНАЧЕ В req.body БУДЕТ undefined
+app.use(express.json()); // * читать JSON-данные из тела запросов req.body
+
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+});
